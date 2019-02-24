d260f775b9bdf3dcb09c2476471abbfaef737ca8
SamuelDspro <47731834+SamuelDspro@users.noreply.github.com>
2019-02-24 12:58:29 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot=commands.Bot(command_prefix='wik')
+
+@bot.event
+async def on_ready():
+    print('Logged in as:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('--------------------')
+    
+@bot.command(pass_context=True)
+async def invite():
+    await bot.say('Help Share our server')
+    await bot.say('https://discord.gg/gyP2UUS')
+    await bot.say('https://discordapp.com/api/oauth2/authorize?client_id=546627382153576455&permissions=0&scope=bot')
+    
+@bot.command(pass_context=True)
+async def mute(ctx,target:discord.Member):
+    role=discord.utils.get(ctx.message.server.roles,name='Muted')
+    
+    await bot.add_roles(target,role)
+    
+@bot.command(pass_context=True)
+async def warn(ctx,target:discord.Member):
+    await bot.send_message(target,'Warning!!')
+    
+@bot.command(pass_context=True)
+async def kick(ctx,target:discord.Member):
+    await bot.kick(target)
+    
+@bot.command(pass_context=True)
+async def ban(ctx,target:discord.Member):
+    await bot.ban(target)
+   
+bot.run('NTQ2NjI3MzgyMTUzNTc2NDU1.D0rcAg.jDVUVS8Wv2Lt3j5AtO3uvmuWL6Eh')
